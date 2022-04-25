afdde85abf0441d9e8b07260742a2628a4d57bf9
DLV <10554724+DLV111@users.noreply.github.com>
2022-04-25 14:39:30 +0930

    Initial Release (#1)
    * Initial commit
    
    * added retries
    
    * Updates to docker file
    
    * minor updates
    
    * update requirements
    
    * fix spelling mistake
    
    * Initial readme.md
    
    * Updated readme a bit
    
    * Updates
    
    * doco update
    
    * doco updates
    
    * update to readme
    
    * update
    
    * .
    
    * typo
    
    * doco updates

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # DripCompound
 Automation to compound drip
+
+This is some basic automation I put together to perform my daily drip compounding.
+
+If you like this please buy me a coffee &/or beer :-) - Airdrop some DRIP or send some BNB/BUSD to my wallet address
+
+`0x1007aaf4b214622155de89546486a070eb731dc0`
+
+If you have any questions you can reach me at https://t.me/Zobah111 
+
+## Configure the .env file
+
+For every wallet you want to use, you must configure the <wallet_name>.env file
+
+See `drip_wallet.env.example` for a full example
+
+Make sure when you are ready for a real run you configure `PERFORM_DRIP_COMPOUNDING=True`
+
+| Variable                    | Usage                                                                                                             | Example                        | Mandatory          |
+|-----------------------------|-------------------------------------------------------------------------------------------------------------------|--------------------------------|--------------------|
+| PRIVATE_KEY                 | The private key of your wallet used to write                                                                      | PRIVATE_KEY=xxxxxxx            | Yes                |
+| WALLET_FRIENDLY_NAME        | The friendly name of your wallet                                                                                  | WALLET_FRIENDLY_NAME=My Wallet | Yes                |
+| PUSHOVER_API_KEY            | API key from your pushover.net account                                                                            | PUSHOVER_API_KEY=xxxx          | No                 |
+| PUSHOVER_USER_KEY           | User key from your pushover.net account                                                                           | PUSHOVER_USER_KEY=xxx          | No                 |
+| PERFORM_DRIP_COMPOUNDING    | If set to True, will perform compounding. Default is not to compound and just test things like notifications etc. | PERFORM_DRIP_COMPOUNDING=True  | No - Default False |
+| MAX_TRIES                   | Number of transaction retries on failure - Default of 1 means it will only try once. Each try, even failure does cost gas fees                               | MAX_TRIES=2                    | No - Default 1     |
+| MIN_BNB_BALANCE             | Min BNB balance to have in your wallet to perform compounding. If less than this it will not perform any actions               | MIN_BNB_BALANCE=0.05            | No - Default 0.02  |
+
+
+## Running the program
+
+My preferred way of running stuff is in docker, makes it easy to ensure it runs everywhere! but you can also adapt this script to run from python with a little effort. If you are running it in native python - then you'll need to edit the code and import the file which has your wallet details in (``load_dotenv(dotenv_path="drip_wallet.env"``)
+
+## Docker
+
+First you'll need to build the image
+
+```
+docker build -f dockerfile -t dripcompound:1.0 .
+```
+
+Then run it using the following command
+
+If you have multiple wallets, have a unique name for each docker image.
+
+```
+# docker start my_wallet_name || docker run -v /etc/localtime:/etc/localtime --env-file my_wallet_name.env --name my_wallet_name -d dripcompound:1.0
+```
+
+### Cron usage example
+
+Run at 9 am every day
+
+```
+0 9 * * * docker start my_docker_run_name || docker run -v /etc/localtime:/etc/localtime --env-file /path/to/my/wallet_info.env --name my_docker_run_name -d dripcompound:1.0
+```
+
+### View the logs
+
+```
+$ docker logs -f drip_test
+2022-04-20 21:57:14,248: Dripping Work v1.0 Started!
+2022-04-20 21:57:14,248: ----------------
+2022-04-20 21:57:14,248: "My Drip Wallet" Selected for processing
+2022-04-20 21:57:15,643: BNB Balance is x.xxx
+2022-04-20 21:57:15,643: Current Balance xx.xxx
+2022-04-20 21:57:15,643: Available to compound xx.xxx
+PushOver Notification
+Title: My Drip Wallet: Drip Compounding
+Body: Current Balance xx.xxx - Compound x.xxx
+2022-04-20 21:57:16,703: Compounding is set to False, only outputting some messages
+2022-04-20 21:57:16,997: Updated Drip balance is: xx.xxx (Increase 0.000)  <-- Note on False this will alway be 0
+PushOver Notification
+Title: My Drip Wallet: Compounding Complete
+Body: Updated Balance xx.xxx (Increase 0.000) - tx test:aaaabbbbccccdddd
+```
+
+## Docker Tips
+
+If you update your env file, make sure you delete the old image eg
+
+```
+docker rm my_docker_run_name
+```
+
+If you want to remove the image every time - use the ``--rm`` command in your docker run - If you do this you won't get a log history - it'll be fresh logs every time - can be useful for testing and the likes!
---
+++ drip_compound.py
@@ -0,0 +1,154 @@
+from web3 import Web3
+import os
+import logging
+import sys
+import time
+from pushover import Client
+from utils import eth2wei, wei2eth, read_json_file, to_checksum
+from dotenv import load_dotenv
+import traceback
+
+DRIP_TOKEN_ADDRESS = "0xFFE811714ab35360b67eE195acE7C10D93f89D8C"
+DRIP_FAUCET_ABI_FILE = "./abis/Faucet.json"
+VERSION = '1.0'
+
+class DripCompoundClass:
+    def __init__(self, perform_compounding=False, txn_timeout=120, gas_price=5, rpc_host="https://bsc-dataseed.binance.org:443",min_balance=0.02, rounding=3, **kwargs):
+
+        MANDATORY_ENV_VARS = ["PRIVATE_KEY",'WALLET_FRIENDLY_NAME']
+
+        for var in MANDATORY_ENV_VARS:
+            if var not in os.environ:
+                raise EnvironmentError("Failed because '{}' env var is not set.".format(var))
+
+        self.private_key = os.environ.pop('PRIVATE_KEY')
+        self.wallet_friendly_name = os.environ.pop('WALLET_FRIENDLY_NAME')
+        self.perform_write_tx = perform_compounding # By default don't do the compounding, just display stuff.
+        logging.info('"%s" Selected for processing' % self.wallet_friendly_name)
+        self.rounding = rounding
+        self.min_balance = min_balance
+        self.txn_timeout = txn_timeout
+        self.gas_price = gas_price
+        self.rpc_host = rpc_host
+        self.InitDripBalance = 0
+
+        # Init the pushover client if defined
+        self.pushover_api_key = os.environ.pop('PUSHOVER_API_KEY', False)
+        self.pushover_user_key = os.environ.pop('PUSHOVER_USER_KEY', False)
+        self.PushOverClientInit()
+
+        # Initialize web3, and load the smart contract objects.
+        self.w3 = Web3(Web3.HTTPProvider(self.rpc_host))
+        self.account = self.w3.eth.account.privateKeyToAccount(self.private_key)
+        self.address = self.account.address
+        self.w3.eth.default_account = self.address
+
+        self.drip_contract = self.w3.eth.contract(
+            to_checksum(DRIP_TOKEN_ADDRESS),
+            abi=read_json_file(DRIP_FAUCET_ABI_FILE))
+
+        self.getDripBalance()
+        self.getAvailableClaims()
+        self.getBNBbalance()
+        self.checkAvailableBNBBalance()
+
+    def getDripBalance(self):
+        self.userInfo = self.drip_contract.functions.userInfo(self.address).call()
+        self.DripBalance = round(wei2eth((self.userInfo[2])),self.rounding)
+        if self.InitDripBalance == 0:
+            self.InitDripBalance = self.DripBalance
+
+    def getDripBalanceIncrease(self):
+        return (self.DripBalance - self.InitDripBalance)
+
+    def getAvailableClaims(self):
+        self.claimsAvailable = round(wei2eth(self.drip_contract.functions.claimsAvailable(self.address).call()),self.rounding)
+
+    def getBNBbalance(self):
+        self.BNBbalance = self.w3.eth.getBalance(self.address)
+        self.BNBbalance = round(wei2eth(self.BNBbalance),self.rounding)
+
+    def checkAvailableBNBBalance(self):
+        if self.BNBbalance > self.min_balance:
+            logging.info('BNB Balance is %s' % round(self.BNBbalance,self.rounding))
+        else:
+            logging.info('Your current BNB balance(%s) is below min required (%s)' % (self.BNBbalance, self.min_balance))
+            self.sendMessage('BNB Balance issue','Your current BNB balance(%s) is below min required (%s) for %s' % (self.BNBbalance, self.min_balance, self.wallet_friendly_name))
+            sys.exit()
+
+    def nonce(self):
+        return self.w3.eth.getTransactionCount(self.address)
+
+    def compoundDrip(self, max_tries=1, retry_sleep=30):
+        remaining_retries = max_tries
+        txn_receipt = None
+        if self.perform_write_tx.lower() == "true":
+            for _ in range(max_tries):
+                try:
+                    remaining_retries+=-1
+                    tx = self.drip_contract.functions.roll().buildTransaction({
+                                        "gasPrice": eth2wei(self.gas_price, "gwei"), "nonce": self.nonce()})
+                    signed_tx = self.w3.eth.account.sign_transaction(tx, self.private_key)
+                    txn = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+                    txn_receipt = self.w3.eth.waitForTransactionReceipt(txn)
+                    if txn_receipt and "status" in txn_receipt and txn_receipt["status"] == 1: 
+                        logging.info("Transaction Successful: %s" % (self.w3.toHex(txn)))
+                        time.sleep(10)
+                        self.getDripBalance()
+                        logging.info("Updated Drip balance is: %s (Increase %s) - tx %s" % (self.DripBalance,self.getDripBalanceIncrease(),self.w3.toHex(txn)))
+                        self.sendMessage("Compounding Complete","Updated Balance %s (Increase %s) - tx %s" % (self.DripBalance,self.getDripBalanceIncrease(),self.w3.toHex(txn)))
+                        break
+                    else:
+                        logging.info("Compounding Failed. %s retries remaining (%s seconds apart). Transaction status '%s' - tx %s" % (remaining_retries,retry_sleep,txn_receipt["status"],self.w3.toHex(txn)))
+                        self.sendMessage("Compounding Failed","%s retries remaining (%s seconds apart). Transaction status '%s' - tx %s" % (remaining_retries,retry_sleep,txn_receipt["status"],self.w3.toHex(txn)))
+                        logging.debug(txn_receipt)
+                        if remaining_retries != 0:
+                            time.sleep(retry_sleep)
+                except:
+                    logging.info(traceback.format_exc())
+                    time.sleep(10)
+        else:
+            logging.info("Compounding is set to False, only outputting some messages")
+            self.getDripBalance()
+            logging.info("Updated Drip balance is: %s (Increase %s)" % (self.DripBalance,self.getDripBalanceIncrease()))
+            self.sendMessage("Compounding Complete","Updated Balance %s (Increase %s) - tx %s" % (self.DripBalance,self.getDripBalanceIncrease(),'test:aaaabbbbccccdddd'))
+
+
+
+    def sendMessage(self, title_txt, body):
+        if self.pushover_api_key and self.pushover_user_key:
+            title_txt = ("%s: %s" % (self.wallet_friendly_name,title_txt) )
+            print("PushOver Notification\n\rTitle: %s\n\rBody: %s" % (title_txt,body))
+            self.client.send_message(body, title=title_txt)
+
+    def PushOverClientInit(self):
+        if self.pushover_api_key and self.pushover_user_key:
+            self.client = Client(self.pushover_user_key, api_token=self.pushover_api_key)
+
+    def setWalletFriendlyName(self,wallet_name="Default Wallet"):
+        self.wallet_name = wallet_name
+
+
+def main():
+    # Setup logger.
+    log_format = '%(asctime)s: %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_format, stream=sys.stdout)
+    logging.info('Dripping Work v%s Started!' % VERSION)
+    logging.info('----------------')
+
+    # Import the env file we're using for testing - if running from CLI - we use docker so don't need it.
+    #load_dotenv(dotenv_path="drip_wallet.env")
+
+    perform_compounding = os.environ.pop('PERFORM_DRIP_COMPOUNDING', "False")
+    min_bnb_balance = float(os.environ.pop('MIN_BNB_BALANCE', 0.02))
+
+    dripwallet = DripCompoundClass(perform_compounding, min_balance=min_bnb_balance)
+
+    logging.info("Current Balance %s" % dripwallet.DripBalance)
+    logging.info("Available to compound %s" % dripwallet.claimsAvailable)
+    dripwallet.sendMessage("Drip Compounding","Current Balance %s - Compound %s" % (dripwallet.DripBalance,dripwallet.claimsAvailable))
+
+    # Actually do the compound step
+    dripwallet.compoundDrip(max_tries=int(os.environ.pop('MAX_TRIES',1)),retry_sleep=30)
+
+main()
